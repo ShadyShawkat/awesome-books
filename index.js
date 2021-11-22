c755f3a87e056447905a4d215db4048590ea6f56
angelacuahutle <angela.cuahutle@gmail.com>
2021-11-22 15:27:02 -0600

    Add removeBook function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const myBooks = [];
 
 const form = document.querySelector('form');
 const booksList = document.querySelector('.books');
+let bookCount = 0;
 
 
 function addBook(e) {
@@ -10,14 +11,21 @@ function addBook(e) {
     const author = document.querySelector('.author').value;
     const Book = {
         title,
-        author
+        author,
+        id:bookCount+1
     }
+    bookCount = bookCount+1
     myBooks.push(Book);
     const bookItem = document.createElement('li');
     bookItem.innerHTML = `
 <p>${Book.title}</p>
 <p>${Book.author}</p>
-<button>Remove</button>`
+<button onclick = "removeBook(${Book.id})">Remove</button>`
 booksList.appendChild(bookItem);
+}
+
+function removeBook(id) {
+    myBooks.splice(id, 1);
+    console.log(myBooks)
 }
 form.addEventListener('submit', addBook);
