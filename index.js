7d3172677b7feb34543383834cdf8795fd0c876f
ayebaishmo <ishmo255super@gmail.com>
2021-11-22 12:15:47 -0800

    Add addBook fuunction that creates an object from form values

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('asd')
+const myBooks = [];
+
+const form = document.querySelector('form');
+const booksList = document.querySelector('.books');
+booksList.innerHTML = `<li>
+<p>titlle</p>
+<p>author</p>
+<button>Remove</button>
+</li>`
+
+function addBook() {
+    const title = document.querySelector('.title');
+    const author = document.querySelector('.author');
+    const Book = {
+        title,
+        author
+    }
+    myBooks.push(Book);
+}
+form.addEventListener('submit', addBook);
