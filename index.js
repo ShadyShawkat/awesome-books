304352054b804efe4f7ba0377120c8421b9db15c
ShadyShawkat <shadyshawkat91@gmail.com>
2021-11-23 19:40:39 +0200

    Add functionality to add book in book class

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const form = document.querySelector('form');
 const booksList = document.querySelector('.books');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
 
-function getDataFromLocalStorage() {
-  return JSON.parse(window.localStorage.getItem('booksData'));
-}
+// function getDataFromLocalStorage() {
+//   return JSON.parse(window.localStorage.getItem('booksData'));
+// }
 
-function setDataInLocalStorage(data) {
-  window.localStorage.setItem('booksData', JSON.stringify(data));
-}
+// function setDataInLocalStorage(data) {
+//   window.localStorage.setItem('booksData', JSON.stringify(data));
+// }
 
 function renderUI(myBooks) {
   booksList.innerHTML = '';
@@ -32,37 +34,88 @@ function removeBook(id) {
 }
 removeBook(null);
 
-const booksData = getDataFromLocalStorage();
+// const booksData = getDataFromLocalStorage();
+// if (!booksData) {
+//   const dataToBeStored = {
+//     myBooks: [],
+//     bookCount: 0,
+//   };
+//   setDataInLocalStorage(dataToBeStored);
+// } else {
+//   renderUI(booksData.myBooks);
+// }
+
+// function addBook(e) {
+  // const title = document.querySelector('.title').value;
+  // const author = document.querySelector('.author').value;
+  // const dataFromLocalStorage = getDataFromLocalStorage();
+  // const Book = {
+  //   title: title.value,
+  //   author: author.value,
+  //   id: dataFromLocalStorage.bookCount + 1,
+  // };
+  // dataFromLocalStorage.myBooks.push(Book);
+  // dataFromLocalStorage.bookCount += 1;
+  // setDataInLocalStorage(dataFromLocalStorage);
+  // const bookItem = document.createElement('li');
+  // bookItem.innerHTML = `
+  //   <p>Title: ${Book.title}</p>
+  //   <p>Author: ${Book.author}</p>
+  //   <button onclick="removeBook(${Book.id})">Remove</button>`;
+  // booksList.appendChild(bookItem);
+  // document.querySelector('.title').value = '';
+  // document.querySelector('.author').value = '';
+// }
+
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id;
+  }
+
+  static getDataFromLocalStorage() {
+    return JSON.parse(window.localStorage.getItem('booksData'));
+  }
+
+  static setDataInLocalStorage(data) {
+    window.localStorage.setItem('booksData', JSON.stringify(data));
+  }
+
+  addBook() {
+    const books = Book.getDataFromLocalStorage();
+    this.id = books.bookCount + 1
+    books.myBooks.push(this);
+    books.bookCount += 1;
+    Book.setDataInLocalStorage(books);
+    renderUI(books.myBooks);
+    title.value = '';
+    author.value = '';
+  }
+
+  // removeBook() {
+    
+  // }
+
+}
+
+
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newBook = new Book(title.value, author.value);
+  newBook.addBook();
+});
+
+const booksData = Book.getDataFromLocalStorage();
 if (!booksData) {
   const dataToBeStored = {
     myBooks: [],
     bookCount: 0,
   };
-  setDataInLocalStorage(dataToBeStored);
+  Book.setDataInLocalStorage(dataToBeStored);
 } else {
   renderUI(booksData.myBooks);
 }
-
-class addBook(e) {
-  e.preventDefault();
-  const title = document.querySelector('.title').value;
-  const author = document.querySelector('.author').value;
-  const dataFromLocalStorage = getDataFromLocalStorage();
-  const Book = {
-    title,
-    author,
-    id: dataFromLocalStorage.bookCount + 1,
-  };
-  dataFromLocalStorage.myBooks.push(Book);
-  dataFromLocalStorage.bookCount += 1;
-  setDataInLocalStorage(dataFromLocalStorage);
-  const bookItem = document.createElement('li');
-  bookItem.innerHTML = `
-    <p>Title: ${Book.title}</p>
-    <p>Author: ${Book.author}</p>
-    <button onclick="removeBook(${Book.id})">Remove</button>`;
-  booksList.appendChild(bookItem);
-  document.querySelector('.title').value = '';
-  document.querySelector('.author').value = '';
-}
-form.addEventListener('submit', addBook);
